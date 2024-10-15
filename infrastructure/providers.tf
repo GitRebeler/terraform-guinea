e24fae19dba52cd96118a9707729831dd2b73887
John Hebeler <john.hebeler@nice.com>
2024-10-15 15:32:18 -0500

    updating provider info

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -4,17 +4,17 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "~>1.5"
+      version = "~>1.14"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.0"
+      version = "~>4.4"
     }
   }
 
   backend "azurerm" {
-      resource_group_name  = "shd-poc-use-terraform-state-01-rg"
-      storage_account_name = "niceterraformstorage"
+      resource_group_name  = "mgt-dev-use-IAC-01-rg"
+      storage_account_name = "mgtdevuse01sa"
       container_name       = "terraform"
       # Key Name <env>.<app>.terraform.tfstate
       key                  = "lab.gp.terraform.tfstate"
