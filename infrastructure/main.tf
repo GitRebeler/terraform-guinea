760d1a8330c586043fa18c18cdf4b818aaa2968a
John Hebeler <john.hebeler@nice.com>
2024-10-17 16:51:54 -0500

    small change tf/az error

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_public_ip" "nice_public_ip" {
   name                = "${local.formatted_name}-publicip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 # Create Network Security Group and rule
